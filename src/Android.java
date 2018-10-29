561db1ae415715b8f9518d88b62f5b214db7f5c5
alvinlukmanNH <galinx20gmail.com>
2018-10-30 00:17:00 +0700

    handphone

---
+++ src/Android.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama        : Alvin Lukman Nulhakim
+ * Nim         : 10117095
+ * Kelas       : IF-3
+ */
+public class Android extends Handphone{
+     private String keyStore;
+
+    public Android(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }
+    
+}
